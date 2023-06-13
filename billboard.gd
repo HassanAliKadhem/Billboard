cc845d0e2d08a18178fc9968342112d64f8e8b91
HassanAliKadhem <79923981+HassanAliKadhem@users.noreply.github.com>
2023-06-13 21:05:14 +0300

    flag scene and drag and drop
    added the flag scene
    addon to fix server issue

--- billboard.gd
+++ billboard.gd
@@ -1,11 +1,19 @@
 extends Node
 
 var billboard: MeshInstance3D;
+var flag: MeshInstance3D;
 
 func change_image(newImage: ImageTexture) -> void:
-	print(newImage);
+	#print(newImage);
 	var new_material: StandardMaterial3D = StandardMaterial3D.new();
 	new_material.albedo_texture = newImage;
-	billboard = get_tree().get_nodes_in_group("plane")[0];
-	print(billboard)
-	billboard.set_surface_override_material(0, new_material);
+	if (get_tree().get_nodes_in_group("plane").size() > 0):
+		billboard = get_tree().get_nodes_in_group("plane")[0];
+		#print(billboard);
+		billboard.set_surface_override_material(0, new_material);
+	elif (get_tree().get_nodes_in_group("flag").size() > 0):
+		flag = get_tree().get_nodes_in_group("flag")[0];
+		#print(flag);
+		flag.get_active_material(0).set_shader_parameter("texture_albedo", new_material.albedo_texture)
+		
+		#flag.set_instance_shader_parameter("texture_albedo", new_material)
--- main.gd
+++ main.gd
@@ -6,18 +6,45 @@ extends Node
 @export var file_name_label: Label;
 var image_file: ImageTexture;
 
+func _ready() -> void:
+	get_viewport().files_dropped.connect(on_files_dropped)
+	_change_level("flag")
+
 func _on_about_button_pressed() -> void:
 	about_dialog.show();
 	about_dialog.popup_centered();
-
 
 func _on_file_button_pressed() -> void:
 	file_dialog.show();
 	file_dialog.popup_centered();
 
+func on_files_dropped(files) -> void:
+	_on_file_dialog_file_selected(files[0])
 
 func _on_file_dialog_file_selected(path: String) -> void:
 	file_name_label.text = "Image: " + path.split("/")[-1].split("\\")[-1];
 	var image: Image = Image.load_from_file(path);
 	image_file = ImageTexture.create_from_image(image);
 	Billboard.change_image(image_file);
+
+func _change_level(name: String) -> void:
+	#print(name)
+	for n in %level.get_children():
+		%level.remove_child(n)
+		n.queue_free()
+	
+	var scene
+	if (name == "flag"):
+		scene = load("res://flag_scene.tscn")
+	else:
+		scene = load("res://billboard_scene.tscn")
+	
+	var instance = scene.instantiate()
+	%level.add_child(instance)
+
+func _on_billboard_button_pressed() -> void:
+	_change_level("billboard")
+
+
+func _on_flag_button_pressed() -> void:
+	_change_level("flag")
